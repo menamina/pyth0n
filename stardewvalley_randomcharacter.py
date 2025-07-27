62cb1b2e18c1c4d6395445bbde96016f66837eb7
Mena <dayjacs03@gmail.com>
2025-07-26 20:56:04 -0500

    listing variables for program

--- stardewvalley_randomcharacter.py
+++ stardewvalley_randomcharacter.py
@@ -1,3 +1,53 @@
 # stardew valley python program that returns a character
 # based on the input of the questions you put in
 # similar to a "what kind of animal are you quiz"
+import random
+
+q1 = input("Seasons: spring, summer, fall, or winter: ")
+q2 = input("1 hobby? art, reading, music, youtube, volunteering, gardening, other: ")
+q3 = input("Drinks: water, juice, wine, beer, whiskey, selter")
+q4 = input("Physical activity: cardio, weights, calesthenics, yoga, none: ")
+q5 = input("Music: pop, punk, rock, reggae, indie, motown, kpop: ")
+
+
+characterList1 = ["Abigail",
+"Alex",
+"Caroline",
+"Clint",
+"Demetrius",
+"Dwarf",
+"Elliott",
+"Emily",
+"Evelyn",
+"George",
+"Gus"]
+
+characterList2 = ["Haley",
+"Harvey",
+"Jas",
+"Jodi",
+"Kent",
+"Krobus",
+"Leah",
+"Lewis",
+"Linus",
+"Marnie",
+"Maru"]
+
+characterList3 = [
+"Pam",
+"Penny",
+"Pierre",
+"Robin",
+"Sam",
+"Sandy",
+"Sebastian",
+"Shane",
+"Vincent",
+"Willy",
+"Wizard"]
+
+print("✨ 🥕 🐇 which stardew valley character suits you most 🥕 🐇 ✨")
+
+
+
